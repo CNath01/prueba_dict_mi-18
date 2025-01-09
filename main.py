1c7acdac2debf321c9b6c00906594e66b8a374e1
CNath01 <92951005+CNath01@users.noreply.github.com>
2025-01-08 19:30:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,5 +6,3 @@ word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
 if word in meme_dict.keys():
     # ¿Qué debemos hacer si se encuentra la palabra?
     print(meme_dict[word])
-else:
-    # ¿Qué hacer si no se encuentra la palabra?
